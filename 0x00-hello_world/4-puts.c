ecdaebe4b252120a7fb210e6a662e746a3e321a5
stephanBull <ndegwastephan@gmail.com>
2022-10-24 06:01:46 -0700

    Puts to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-/*
- * main - Print a line
+/**
+ * main -Print a line of code using puts
  *
- * return: 0
+ * Return: 0
 */
+
 int main(void)
 	{
 		puts("\"Programming is like building a multilingual puzzle");
