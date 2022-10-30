41e985dae077a6a919790c138d9fcafefb750030
stephanBull <ndegwastephan@gmail.com>
2022-10-30 16:15:33 -0700

    Print alphabet without

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print alphabet in lowercase
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
