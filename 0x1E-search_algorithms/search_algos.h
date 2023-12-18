a3d0cf21d5d134e3a7f48747d299bf285e0d8805
StephanBull <ndegwastephan@gmail.com>
2023-12-18 09:28:56 +0000

    Search algorithms tasks

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_ALGO
+#define SEARCH_ALGO
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
